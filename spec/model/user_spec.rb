68ebc528e28c3a2ab556af3464092db911d36379
Gedalias dos Santos Caldas <gsantos.c18@gmail.com>
2024-12-06 21:59:36 -0300

    chore: improvement rubocop rules

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -6,7 +6,6 @@ require './app/model/user'
 RSpec.describe User do
   context 'with user' do
     let(:user) { described_class.new(name: 'John') }
-
     it { expect(user.name).to eq('John') }
   end
 end
